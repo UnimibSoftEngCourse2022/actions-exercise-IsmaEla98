09ce9034eafc6e507610524d72a010a905a2657a
Ismail El Abiad <ismail.elabiad1998@gmail.com>
2022-01-08 23:39:33 +0100

    prova

--- src/main/java/ilaria/travistest/App.java
+++ src/main/java/ilaria/travistest/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
     	if(args.length != 0) {
-    		System.out.println( "Hello " + args[0 ); 		
+    		System.out.println( "Hello " + args[0] ); 		
     	} else {
     		System.out.println( "What's your name????");
     		System.out.println( "I would love to see travis working...");
